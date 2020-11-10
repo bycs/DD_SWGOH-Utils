e4b6d189384a4c886dc9cfae346ecd31c14865ab
Stas Cartoon <bycs@mail.ru>
2020-11-10 13:55:24 +0300

    Variables in the virtual environment

--- SWGOH_Utils/settings.py
+++ SWGOH_Utils/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from tokens.tokens import DJANGO_SECRET_KEY
+import os
+DJANGO_SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
