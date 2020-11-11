0526528b5b29d1de91e06ed7a3d828ed590d7fd8
Stas Cartoon <bycs@mail.ru>
2020-11-11 06:59:45 +0300

    Update settings.py

--- SWGOH_Utils/settings.py
+++ SWGOH_Utils/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
