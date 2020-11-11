647ccde9e454fe95d3632fdf7d3a29a139a515ea
Stas Cartoon <bycs@mail.ru>
2020-11-11 07:40:50 +0300

    Update settings.py

--- SWGOH_Utils/settings.py
+++ SWGOH_Utils/settings.py
@@ -21,6 +21,7 @@ DB_HOST = os.environ['DB_HOST']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
