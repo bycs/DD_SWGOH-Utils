71e7b6ed537510699e65cf87116d5540b03cb67c
Stas Cartoon <bycs@mail.ru>
2021-04-08 23:52:49 +0300

    setting up default_auto_field

--- about_players/apps.py
+++ about_players/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AboutPlayersConfig(AppConfig):
     name = "about_players"
+    default_auto_field = "django.db.models.AutoField"
     verbose_name = "About Players"
--- swgoh_guilds/im/apps.py
+++ swgoh_guilds/im/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ImConfig(AppConfig):
     name = "swgoh_guilds.im"
+    default_auto_field = "django.db.models.AutoField"
     verbose_name = "Imperial Military"
