dfb36a82b211e339e1274772ef435eb62e65de92
Stas Cartoon <bycs@mail.ru>
2021-05-18 22:11:16 +0300

    refactoring

--- swgoh_guilds/im/urls.py
+++ swgoh_guilds/im/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-import swgoh_guilds.im.views
+from swgoh_guilds.im import views
 
 urlpatterns = [
-    path("", swgoh_guilds.im.views.im_index),
-    path("data/", swgoh_guilds.im.views.data),
-    path("sync_guld_data/", swgoh_guilds.im.views.sync_guld_data),
+    path("", views.im_index, name="im_index"),
+    path("data/", views.data, name="im_data"),
+    path("sync_guld_data/", views.sync_guld_data, name="im_sync_guld_data"),
 ]
--- swgoh_guilds/im/views.py
+++ swgoh_guilds/im/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-import swgoh_sync.models.guilds
-import swgoh_guilds.im.models
+from swgoh_sync.models import guilds
+from swgoh_guilds.im import models
 
 
 def im_index(request):
@@ -10,13 +10,13 @@ def im_index(request):
 
 def data(request):
     guild_name = "Imperial Military"
-    players = swgoh_guilds.im.models.Data.guild_players_data_manager.all()
+    players = models.Data.guild_players_data_manager.all()
     return render(
         request, "im_data.html", {"guild_name": guild_name, "players": players}
     )
 
 
 def sync_guld_data(request):
-    swgoh_sync.models.guilds.GuildsData.guilds_data_managers.sync_to_db(8187)
-    swgoh_guilds.im.models.Data.guild_players_data_manager.sync_to_db(8187)
+    guilds.GuildsData.guilds_data_managers.sync_to_db(8187)
+    models.Data.guild_players_data_manager.sync_to_db(8187)
     return render(request, "im_index.html")
